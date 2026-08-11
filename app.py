5a12089cb6212325719718046fbabedb0cf2263e
d.osuna@uniandes.edu.co <d.osuna@uniandes.edu.co>
2026-08-10 21:37:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import pandas as pd
 import datetime as dt
 
 
-
+#docstings de  prueba
 app = dash.Dash(
     __name__,
     meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
